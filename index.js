81f87cbea1f6cb5bb159be02ed01857eab6c14b7
thanhnguyen07 <thanhjang2k@gmail.com>
2023-06-01 06:54:02 +0700

    update

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const fs = require('fs');
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 
-// const corsOptions = {
-//     origin: '*',
-//     credentials: true,
-//     optionSuccessStatus: 200,
-// };
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+};
 
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
+// app.use(cors());
+
 // Connect DB
 const db = require('./src/config/db');
 db.connect();
@@ -28,46 +29,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'src/public')));
 
 app.use(methodOverride('_method'));
-
-// app.get('/video', function (req, res) {
-//     console.log(req.query);
-//     const path = `src/public/video/${req.query.id}.mp4`;
-//     const stat = fs.statSync(path);
-//     const fileSize = stat.size;
-//     const range = req.headers.range;
-
-//     if (range) {
-//         const parts = range.replace(/bytes=/, '').split('-');
-//         const start = parseInt(parts[0], 10);
-//         const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-
-//         if (start >= fileSize) {
-//             res.status(416).send(
-//                 'Requested range not satisfiable\n' + start + ' >= ' + fileSize,
-//             );
-//             return;
-//         }
-
-//         const chunksize = end - start + 1;
-//         const file = fs.createReadStream(path, {start, end});
-//         const head = {
-//             'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-//             'Accept-Ranges': 'bytes',
-//             'Content-Length': chunksize,
-//             'Content-Type': 'video/mp4',
-//         };
-
-//         res.writeHead(206, head);
-//         file.pipe(res);
-//     } else {
-//         const head = {
-//             'Content-Length': fileSize,
-//             'Content-Type': 'video/mp4',
-//         };
-//         res.writeHead(200, head);
-//         fs.createReadStream(path).pipe(res);
-//     }
-// });
 
 app.get('/', (req, res) => {
     res.send('SERVER ON');
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,17 +5,12 @@ const token = require('../utils/token');
 
 router.get('/', adminController.Films);
 router.post('/login', adminController.login);
-// router.post('/details', token.authenToken, filmsController.details);
-// router.get('/create', filmsController.create);
 router.get('/users', adminController.viewUsers);
 router.get('/:_id', adminController.details);
 router.post('/store', adminController.store);
-// router.delete('/:_id', filmsController.delete)
-// router.get('/', filmsController.Films)
 router.post('/deleteuser', token.authenToken, adminController.deleteuser);
 router.get('/userdata', token.authenToken, adminController.userdata);
 router.post('/getdatauser', token.authenToken, adminController.getProfile);
 router.post('/updateuser', token.authenToken, adminController.updateuser);
-
 
 module.exports = router;
